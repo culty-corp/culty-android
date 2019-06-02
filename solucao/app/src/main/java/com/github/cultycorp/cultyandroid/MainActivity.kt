5d9542e9773d817102b970bb168494afdbe3b261
saulocalixto <sauloacalixto@gmail.com>
2019-06-02 15:49:27 -0300

    Adicionando ação ao cliar nos itens do menu
    Adicionando ações ao clicar nos itens do menu, posteriormente modificar para que essas ações chamem outras páginas.

--- solucao/app/src/main/java/com/github/cultycorp/cultyandroid/MainActivity.kt
+++ solucao/app/src/main/java/com/github/cultycorp/cultyandroid/MainActivity.kt
@@ -1,25 +1,60 @@
 package com.github.cultycorp.cultyandroid
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import android.widget.Toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        drawer = findViewById(R.id.drawer_layout)
+
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        var toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.abrirOpcoes, R.string.fecharOpcoes)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
 /*        fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }*/
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var id = item.itemId
+        when (id) {
+            R.id.nav_explore -> Toast.makeText(this, "Explorar tudo aqui!", Toast.LENGTH_SHORT).show()
+            R.id.nav_following -> Toast.makeText(this, "Vamos seguir todo mundo!", Toast.LENGTH_SHORT).show()
+            R.id.nav_artists -> Toast.makeText(this, "Artistas de araque!", Toast.LENGTH_SHORT).show()
+            R.id.login -> Toast.makeText(this, "Vamos entrando!", Toast.LENGTH_SHORT).show()
+            R.id.login -> Toast.makeText(this, "Vamos entrando!", Toast.LENGTH_SHORT).show()
+            R.id.upload -> Toast.makeText(this, "Vamos mandar trem!", Toast.LENGTH_SHORT).show()
+            R.id.profile -> Toast.makeText(this, "Olha seu perfil!", Toast.LENGTH_SHORT).show()
+            R.id.logout -> Toast.makeText(this, "Vai embora, ingrato!", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(Gravity.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(Gravity.START)) drawer.closeDrawer(Gravity.START) else super.onBackPressed()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
