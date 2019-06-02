4cd7ff6fb61891786ef63f3423885471a42b4ca7
saulocalixto <sauloacalixto@gmail.com>
2019-06-02 10:49:23 -0300

    Criação de navbar
    Colocando a navbar na activty main, removendo toolbar que não será usada nesse contexto.

--- solucao/app/src/main/java/com/github/cultycorp/cultyandroid/MainActivity.kt
+++ solucao/app/src/main/java/com/github/cultycorp/cultyandroid/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,12 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        fab.setOnClickListener { view ->
+/*        fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
