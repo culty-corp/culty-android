17138a83a905d2247469979a586a25ec78a961e3
saulocalixto <sauloacalixto@gmail.com>
2019-07-02 23:23:48 -0300

    Acrescentando chamadas da api.

--- solucao/src/actions/index.js
+++ solucao/src/actions/index.js
@@ -1,3 +1,5 @@
+import * as Api from "../utils/api";
+
 export const SIGNEDIN = 'SIGNEDIN'
 export const SIGNEDOUT = 'SIGNEDOUT'
 export const SAVEPOSTS = 'SAVEPOSTS'
@@ -26,3 +28,117 @@ export function savePosts(posts) {
     payload: posts
   }
 }
+
+export const passarPost = () => {
+  return {
+    type: "PASSAR_POST",
+  };
+};
+
+export const adicionarPost = post => {
+  return {
+    type: "ADICIONAR_POST",
+    post
+  };
+};
+
+export const fetchAdicionarPost = (post) => dispatch =>
+Api.createObra(post).then(
+  obras => {
+    dispatch(adicionarPost(post));
+  },
+  erro => console.log(`Erro na requisição: ${erro}`)
+);
+
+export const adicionarUsuario = post => {
+  return {
+    type: "ADICIONAR_USUARIO",
+    post
+  };
+};
+
+export const fetchAdicionarUsuario = (usuario) => dispatch =>
+Api.createUsuario(usuario).then(
+  obras => {
+    dispatch(adicionarUsuario(usuario));
+  },
+  erro => console.log(`Erro na requisição: ${erro}`)
+);
+
+export const atualizarUsuario = usuario => {
+  return {
+    type: "ATUALIZAR_USUARIO",
+    usuario
+  };
+};
+
+  export const fetchAtualizarUsuario = (usuario) => dispatch => (
+    Api.updateUsuario(usuario).then(() => {
+      dispatch(atualizarUsuario(usuario))
+    }, erro => console.log(`Algo de errado não deu certo: ${erro}`))
+  );
+
+export const addFiltro = filtro => {
+  return {
+    type: "ADD_FILTRO",
+    filtro
+  };
+};
+
+export const removeObra = idObra => {
+  return {
+    type: "REMOVE_OBRA",
+    idObra
+  }
+}
+
+export const fetchRemoveObra = (idObra) => dispatch =>
+Api.deleteObra(idObra).then(
+  idObra => {
+    dispatch(removeObra(idObra));
+  },
+  erro => console.log(`Erro na requisição: ${erro}`)
+);
+
+export const removeFiltro = filtro => {
+  return {
+    type: "REMOVE_FILTRO",
+    filtro
+  };
+};
+
+export const logar = (login) => {
+  return {
+    type: "LOGAR",
+    login
+  };
+};
+
+export const fetchLogar = (login) => dispatch =>
+Api.efetueLogin(login).then(
+  login => {
+    dispatch(logar(login));
+  },
+  erro => console.log(`Erro na requisição: ${erro}`)
+);
+
+export const deslogar = () => {
+  return {
+    type: "DESLOGAR"
+  };
+};
+
+export const getAllObras = obras => {
+  return {
+    type: "GET_ALL_OBRAS",
+    obras
+  };
+};
+
+export const fetchGetAllObras = () => dispatch =>
+  Api.getAllObras().then(
+    obras => {
+      dispatch(getAllObras(obras));
+    },
+    erro => console.log(`Erro na requisição: ${erro}`)
+  );
---
+++ solucao/src/utils/api.js
@@ -0,0 +1,96 @@
+const api = "https://sifo.tech/culty";
+const headers = {
+  "Content-Type": "application/json"
+};
+
+export const getAllUsuarios = () =>
+  fetch(`${api}/usuarios`, {
+    headers,
+    method: "GET"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const getUsuario = id =>
+  fetch(`${api}/usuarios/${id}`, {
+    headers,
+    method: "GET"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const deleteUsuario = id =>
+  fetch(`${api}/usuarios/${id}`, {
+    headers,
+    method: "DELETE"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const createUsuario = usuario =>
+  fetch(`${api}/usuarios`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(usuario)
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const updateUsuario = usuario =>
+  fetch(`${api}/usuarios/${usuario.id}`, {
+    method: "PUT",
+    headers,
+    body: JSON.stringify(usuario)
+  })
+    .then(res => res.json());
+
+export const getAllObras = () =>
+  fetch(`${api}/obras`, {
+    headers,
+    method: "GET"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const getObra = id =>
+  fetch(`${api}/obras/${id}`, {
+    headers,
+    method: "GET"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const deleteObra = id =>
+  fetch(`${api}/obras/${id}`, {
+    headers,
+    method: "DELETE"
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const createObra = obra =>
+  fetch(`${api}/obras`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(obra)
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const efetueLogin = login =>
+  fetch(`${api}/login`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(login)
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const updateObra = obra =>
+  fetch(`${api}/obras`, {
+    method: "PUT",
+    headers,
+    body: JSON.stringify(obra)
+  })
+    .then(res => res.json())
+    .then(data => data);
